47776f64edf13037f245968b1009a0d6d9c895de
KeyboardCowboy <chris.albrecht@lullabot.com>
2024-02-23 11:37:50 -0700

    Handle multiple levels.

--- .tugboat/compile-vars.js
+++ .tugboat/compile-vars.js
@@ -2,54 +2,84 @@
  * Generate a markdown file featuring all the environment variables and their example values.
  */
 
-const fs = require('fs');
-const Yaml = require('js-yaml');
+const fs = require("fs");
+const Yaml = require("js-yaml");
 
-const sourceFile = '.tugboat/env-vars.yml';
-const destFile = 'public/vars.md';
+const sourceFile = ".tugboat/env-vars.yml";
+const destFile = "public/vars.md";
 const output = [];
+
+const Renderer = {
+  printSection: (section, headerLevel) => {
+    const head = "".padStart(headerLevel, "#");
+
+    // Print the section name.
+    output.push(`${head} ${section.name}`);
+    output.push("");
+
+    // Add the section description.
+    if (typeof section.desc !== "undefined") {
+      output.push(`${section.desc}`);
+      output.push("");
+    }
+
+    // If this section has subsections, add a level.
+    if (typeof section.sections !== "undefined") {
+      // Iterate over the sections to print the headings.
+      section.sections.forEach(section => {
+        Renderer.printSection(section, 3);
+      });
+    }
+
+    if (typeof section.vars !== "undefined") {
+      Renderer.printVars(section.vars);
+    }
+  },
+
+  printVars: (vars) => {
+    // Protect against empty sections.
+    vars = vars || [];
+
+    // Create the table headings.
+    output.push("| Variable Name | Description | Example Value |");
+    output.push("|:--------------|:------------|:--------------|");
+
+    // Iterate over all variables in each section.
+    vars.forEach(variable => {
+      // Ensure we have a value so the app doesn't die.
+      let envVarVal = Renderer.getEnvVarVal(variable);
+
+      // Add the row to the table.
+      output.push(`| **\`${variable.name}\`** | ${variable.desc} | ${envVarVal} |`);
+    });
+
+    output.push("");
+  },
+
+  getEnvVarVal: (envVar) => {
+    let envVarValName = typeof envVar.override !== 'undefined' ? envVar.override : envVar.name;
+    let envVarValRefName = `process.env.${envVarValName}`;
+
+    let envVarVal = "";
+    if (typeof eval(envVarValRefName) !== "undefined") {
+      envVarVal = '`' + eval(envVarValRefName) + '`';
+    }
+
+    return envVarVal;
+  }
+};
 
 try {
   // Read the variable data.
-  const data = Yaml.load(fs.readFileSync(sourceFile, 'utf8'));
+  const data = Yaml.load(fs.readFileSync(sourceFile, "utf8"));
 
   // Iterate over the sections to print the headings.
   data.sections.forEach(section => {
-    // Print the section name.
-    output.push(`## ${section.name}`);
-    output.push('');
-
-    // Add the section description.
-    if (typeof section.desc !== 'undefined') {
-      output.push(`${section.desc}`);
-      output.push('');
-    }
-
-    // Protect against empty sections.
-    section.vars = section.vars || [];
-
-    // Create the table headings.
-    output.push('| Variable Name | Description | Example Value |');
-    output.push('|:--------------|:------------|:--------------|');
-
-    // Iterate over all variables in each section.
-    section.vars.forEach(variable => {
-      // Ensure we have a value so the app doesn't die.
-      let envVarName = `process.env.${variable.name}`;
-      let envVarVal = '';
-      if (typeof eval(envVarName) !== 'undefined') {
-        envVarVal = eval(envVarName);
-      }
-
-      // Add the row to the table.
-      output.push(`| **\`${variable.name}\`** | ${variable.desc} | \`${envVarVal}\` |`);
-    });
-
-    output.push('');
+    Renderer.printSection(section, 2);
   });
 
   // Write the file.
-  fs.writeFileSync(destFile, output.join('\n'));
+  fs.writeFileSync(destFile, output.join("\n"));
   console.log(`Wrote variables to ${destFile}`);
 } catch (e) {
   console.log(e);
